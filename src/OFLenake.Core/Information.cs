4d3ea374bd2fb5ed4d449c6a61dafdc6f018fa16
OFLenake <dev@onkgopotselenake.me>
2023-04-22 12:53:17 +0200

    Add project files.

---
+++ src/OFLenake.Core/Information.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Onkgopotse Lenake.
+// Licensed under the MIT License.
+
+namespace OFLenake.Core
+{
+    public class Information
+    {
+
+        private static readonly string FirstName = "Onkgopotse";
+        private static readonly char[] Initials = {'O', 'F'};
+        private static string? Surname = "Lenake";
+
+        //public string? GetFirstName { get; }
+
+        /// <summary>
+        /// <see cref="ToString"/> method returns a string
+        /// representing a <see cref="Information"/> object.
+        /// </summary>
+        /// <returns>
+        /// A string representing a <see cref="Information"/> object.
+        /// </returns>
+        public override string ToString()
+        {
+            string result = $"Profile Information:{Environment.NewLine}" +
+                            $"First name: {FirstName}{Environment.NewLine}" +
+                            $"Initials: {Initials}{Environment.NewLine}" +
+                            $"Surname: {Surname}{Environment.NewLine}";
+            return $"{result}";
+        }
+    }
+}
---
+++ src/OFLenake/OFLConsole.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Onkgopotse Lenake.
+// Licensed under the MIT License.
+
+using OFLenake.Core;
+
+namespace OFLenake
+{
+    /// <summary>
+    /// OFLenake console runner application.
+    /// </summary>
+    public class OFLConsole
+    {
+        private static readonly string AppTitle = $"OFLenake.OFLConsole";
+        private static Information? OFLInfo = null;
+
+        /// <summary>
+        /// OFLenake console runner application main method.
+        /// </summary>
+        /// <param name="args">
+        /// <see cref="string"/>[] objects to parse to the
+        /// <see cref="Main"/> method.
+        /// </param>
+        /// <remarks>
+        /// <para>
+        /// The <see cref="Main"/> method is the main entry point
+        /// to the <see cref="OFLConsole"/> runner application.
+        /// </para>
+        /// </remarks>
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{AppTitle} started...\n");
+
+            OFLInfo = new Information();
+            Console.WriteLine($"{OFLInfo}\n");
+
+            Console.WriteLine($"{AppTitle} finished...\n");
+        }
+    }
+}
